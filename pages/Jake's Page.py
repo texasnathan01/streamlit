039b9d65d9c727690c1957f9efba3675ac44745d
jakegartrell <90012844+jakegartrell@users.noreply.github.com>
2024-02-26 00:27:00 -0600

    lol

--- pages/Jake's Page.py
+++ pages/Jake's Page.py
@@ -6,7 +6,7 @@ from skimage.transform import swirl
 from skimage.color import rgb2gray
 from PIL import Image
 
-def get_video():
+def get_video(swirl_strength, threshold, edge):
 
     while cap.isOpened() and not stop_button_pressed:
 
@@ -14,7 +14,17 @@ def get_video():
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         if sobel_pressed:
-            frame = np.round((1 - np.array(sobel(frame)))*255).astype(np.uint8)
+             
+            if edge == "Basic Edge :movie_camera:":
+                frame = rgb2gray(frame)
+                frame = np.round((1 - np.array(sobel(frame)))*255).astype(np.uint8)
+            
+            elif edge == "***Evil Edge***":
+                frame = np.round((np.array(sobel(frame)))*255).astype(np.uint8)
+            
+            else:
+                frame = np.round((1 - np.array(sobel(frame)))*255).astype(np.uint8)
+                
             min=np.min(frame)
             max=np.max(frame)
 
@@ -23,27 +33,49 @@ def get_video():
             frame = Image.fromarray(LUT[frame])
 
         if swirl_pressed:
-            frame = swirl(frame, rotation=0, strength=10, radius=120)
+            frame = swirl(frame, rotation=0, strength=2, radius=swirl_strength)
 
 
         if grey_pressed:
             frame = rgb2gray(frame)
+            if threshold:
+                frame = np.round((1 - np.array(frame))*255).astype(np.uint8)
 
         frame_placeholder.image(frame, channels="RGB")
 
 frame_placeholder = st.empty()
 frame_placeholder.image("Crazy Camera Landing.png", channels="RGB")
 
-st.title("Crazy Video Camera App")
-start_button_pressed = st.button("Start")
-sobel_pressed = st.button("Edgey")
-grey_pressed = st.button("Grey")
-swirl_pressed = st.button("Swirl")
-stop_button_pressed = st.button("Stop")
+st.title("Crazy Video Camera App Options")
+st.write("")
+
+col1, col2, col3, col4, col5 = st.columns([1,1,1,1,1])
+
+with col1:
+    start_button_pressed = st.button("Normal")
+
+with col2:
+    sobel_pressed = st.button("Edgey")
+    edge = st.radio("",["Basic Edge :movie_camera:","***Evil Edge***", ":rainbow[Color Edge]"])
+    
+with col3:
+    grey_pressed = st.button("Gray")
+    st.write("")
+    threshold = st.toggle("Invert")
+
+with col4:
+    swirl_pressed = st.button("Swirl")
+    st.write("")
+    swirl_strength = st.select_slider("Strength of Swirl",
+                                      options=[100, 200, 300, 400, 500])
+
+with col5:
+    stop_button_pressed = st.button("Stop")
+
 
 if start_button_pressed | sobel_pressed | grey_pressed | swirl_pressed:
     cap = cv2.VideoCapture(0)
-    get_video()
+    get_video(swirl_strength, threshold, edge)
 
 if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
     st.text("The video has stopped")
