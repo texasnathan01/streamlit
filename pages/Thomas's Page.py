732d25be076e2ce9c68f8da8e4308bc4f9df001e
tasimms33-TAMU <119848158+tasimms33-TAMU@users.noreply.github.com>
2024-02-25 21:04:09 -0600

    Added better GUI to Doom Page

--- pages/Thomas's Page.py
+++ pages/Thomas's Page.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from streamlit.components.v1 import html
 
+st.set_page_config(page_title='Can it run Doom?', page_icon="./Images/DoomIcon.png")
+
 doom_string = """
- <!doctype html>
- <html lang="en-us">
    <head>
      <meta charset="utf-8">
      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
      <title>DOOM</title>
      <style type="text/css">
-       .dosbox-container { width: 320px; height: 200px; }
+       .dosbox-container { width: 640px; height: 400px;}
      </style>
    </head>
    <body>
@@ -30,7 +30,24 @@ doom_string = """
        });
      </script>
    </body>
- </html>
 """
 
+st.markdown("<h1 style='text-align: center; color: black;'>Can it run <font color=red>DOOM</font>?</h1>", unsafe_allow_html=True)
+
 html(doom_string, width=640, height=400)
+
+
+st.markdown("""
+    <h3 id="controls">Controls:</h3>
+    <ul>
+        <li>Move: UP, DOWN, LEFT, RIGHT</li>
+        <li>Use: W</li>
+        <li>Fire: S</li>
+        <li>Speed on: SPACE</li>
+        <li>Strafe on: ALT</li>
+        <li>Strafe: A, D</li>
+        <li>Menu Select: Enter</li>
+    </ul>
+""", unsafe_allow_html=True)
+
+st.markdown('<p></p><p style="text-align: left">"It Runs Doom" is an expression indicating that a device is capable of running the 1993 first-person shooter video game Doom, which has been successfully ported to a variety of electronic gadgets designed for purposes other than gaming.</p>', unsafe_allow_html=True)
